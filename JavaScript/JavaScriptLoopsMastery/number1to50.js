60c2dc512d7b0a6951c24a2277f5bcd66872f823
bablu kumar <bablu707041dk@gmail.com>
2026-01-03 19:37:04 +0530

    todays Final Commt

---
+++ JavaScript/JavaScriptLoopsMastery/number1to50.js
@@ -0,0 +1,10 @@
+
+console.log("Hello welcome to my world ... ");
+let x = "";
+for(let a = 1;a<=50;a++){
+    if(a % 3 ==0){
+        x = x + a+",";
+    }
+}
+console.log(x)
+console.log("here is the numebr they can divisible by 3 ... ")
---
+++ JavaScript/JavaScriptLoopsMastery/whileLoop.js
@@ -0,0 +1,9 @@
+console.log("Hello Welcome to  The My World...");
+
+let i=1;
+let x = "";
+while(i!=32){
+    x = x + i +",";
+    i++;
+}
+console.log(x);
